34bee1f62ad8d82b339f7fde5278de714109b571
Chris <ckgeorge23@gmail.com>
2019-01-16 19:26:05 -0600

    Update app.js

--- app.js
+++ app.js
@@ -3,13 +3,23 @@
     var hiddenInfo = $(".hidden-info");
     var readMoreTrigger = $(".rm");
 		//hide div .hidden-info
-		hiddenInfo.hide(); 
-	  readMoreTrigger.on("click", function(){
+		hiddenInfo.hide();
+
+	  readMoreTrigger.on("click", function(event){
+
+	  	// 
 		//toggle the hidden div
-	    hiddenInfo.toggle();
+	    // hiddenInfo.toggle();
+
+	    var hiddenElem = $(event.target).parent().find('.hidden-info');
+	    $(hiddenElem).toggle();
+
 			//toggle text in .hidden-info to 'Read Less', 'Read More'
-      if (readMoreTrigger.text() == "Read More")
-        readMoreTrigger.text("Read less");
-				else readMoreTrigger.text("Read More");
+      	if ($(event.target).text() == "Read More") {
+        	$(event.target).text("Read less");
+    	} else {
+    		$(event.target).text("Read More");
+    	}
+		
     });
   });
